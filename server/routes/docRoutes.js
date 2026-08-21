2e423a382245ffbda3bc7f372c97c35655205616
Issam <eddahbyissam265@gmail.com>
2026-08-22 01:32:41 +0200

    Fix: Réparation de admin.js, ajout des documents de concours et correction table DB

--- server/routes/docRoutes.js
+++ server/routes/docRoutes.js
@@ -2,13 +2,22 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs'); // 👈 NOUVEAU : Le module pour créer des dossiers !
 const docController = require('../controllers/docController');
+
+// 🌟 SÉCURITÉ : Le chemin GPS exact du dossier
+const uploadDir = path.join(__dirname, '../../client/uploads');
+
+// 🌟 NOUVEAU : Si le dossier n'existe pas ou qu'il est mal placé, le serveur le crée tout seul !
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+    console.log("📁 Dossier 'uploads' créé automatiquement !");
+}
 
 // 🌟 CONFIGURATION DE MULTER POUR LES DOCUMENTS (PDF)
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        // On va dans le dossier client/uploads
-        cb(null, 'client/uploads/'); 
+        cb(null, uploadDir); // On utilise l'adresse absolue
     },
     filename: function (req, file, cb) {
         cb(null, 'doc-' + Date.now() + path.extname(file.originalname));
